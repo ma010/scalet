d1b841c7270171a84222191921b3ea043bcc16bc
Nathan Ma <nathanxma@users.noreply.github.com>
2015-03-25 20:25:39 -0500

    added comments to PascalTriangle.scala

--- PascalTriangle.scala
+++ PascalTriangle.scala
@@ -11,10 +11,29 @@
 ]
  */
 
-
+/**
+ * This function takes one parameter which specifies
+ * the number of rows of Pascal's triangle
+ */
 def pascaltri (k: Int) {
+  // rows is a Range traversable
   val rows = 0 until k
+  
+  /**
+   * ptri is a two dimensional array filled with 1, e.g.:
+     [1]
+    [1,1]
+   [1,1,1]
+  [1,1,1,1]
+ [1,1,1,1,1]
+   */
   val ptri = rows.toArray map (x => Array.fill(x+1)(1))
+  
+  /**
+   * iterate through the 2-D array and 
+   * calculate based on the principle
+   * of Pascal's triangle
+   */
   if (rows.length >= 2) {
     for {
       row <- ptri.indices.init
@@ -24,9 +43,15 @@ def pascaltri (k: Int) {
       ptri(row+1)(col+1) = ptri(row)(col) + ptri(row)(col+1)
     }  
   }
+  
+  /**
+   * display Pascal's triangle in
+   * a specific format
+   */  
   println("[")
   for {row <- ptri.indices} {
     if (row < ptri.indices.last)
+      // mkString(start, sep, end)
       println(ptri(row).mkString(" "*(ptri.length-row)+"[", ",", "],"))
     else
       println(ptri(row).mkString(" "*(ptri.length-row)+"[", ",", "]"))
