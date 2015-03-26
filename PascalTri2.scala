9b7641c7a86ac2c9413c3961cb8ddf9fe28a67c0
Nathan Ma <nathanxma@users.noreply.github.com>
2015-03-25 19:59:37 -0500

    updated PascalTri2.scala

--- PascalTri2.scala
+++ PascalTri2.scala
@@ -1,28 +1,19 @@
 
-
-val allRows = new Array[Array[Int]](5)
-for {
-  row <- allRows.indices
-  //num <- row  
-} 
-{
-  allRows(row) = new Array[Int](row+1)
-  for {
-    row 
+def pascaltri (k: Int) {
+  val rows = 0 until k
+  val ptri = rows.toArray map (x => Array.fill(x+1)(1))
+  if (rows.length >= 2) {
+    for {
+      row <- ptri.indices.init
+      if (row >= 1)
+      col <- ptri(row).indices.init
+    } {
+      ptri(row+1)(col+1) = ptri(row)(col) + ptri(row)(col+1)
+    }  
   }
-  println(allRows(row).length)
+  for {row <- ptri.indices}
+  println(ptri(row).mkString(" "*(ptri.length-row)+"[", ",", "]"))
 }
 
 
-//
-//for (line <- 1 until 6) {
-//  allRows = allRows :+ Array.fill(line)(1)
-//  println(allRows.length)
-//  //for (num <- 1 until (5-2) if (5 > 2)){
-//  //  allRows(line-1)(num) = allRows(line-2)(num-1) + allRows(line-2)(num)
-//  //}
-//}
-
-
-//val pascalTri = rows(5)
-//println(pascalTri)
+pascaltri(5)
