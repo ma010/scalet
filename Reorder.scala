11de2484f34e220d1c600f6bf696cde8aa30a908
Nathan Ma <nathanxma@users.noreply.github.com>
2015-03-26 08:01:03 -0500

    added comments to Reoder.scala

--- Reorder.scala
+++ Reorder.scala
@@ -5,6 +5,16 @@
  * For example, a list (1, 2, 3, 4, 5, 6, 7, 8, 9, 10) should
  * be reordered into (1, 10, 2, 9, 3, 8, 4, 7, 5, 6)
  */
+/*
+ * According to scala List's basic structure,
+ * a new element (single-member list) can only
+ * be prepended to the beginning of the list.
+ * Therefore, in y :: ys, y is an element while
+ * ys can be element(s) of list.
+ * Double colons (::) means "construct", 
+ * pronounced as "cons".
+ */
+ */
 
 def reorder (xs: List[Int]): List[Int] = xs match {  
   case List() => List() // empty list doesn't need reordering
