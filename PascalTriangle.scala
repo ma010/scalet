804076600aef0a73fe21f12878bdc1b3dee41fd3
Nathan Ma <nathanxma@users.noreply.github.com>
2015-03-29 10:35:54 -0500

    moved display method from Triangle.scala back to PascalTriangle.scala

--- PascalTriangle.scala
+++ PascalTriangle.scala
@@ -1,72 +1,57 @@
-package PascalTriangle
 /**
  * Given numRows, generate the first numRows of Pascal's triangle.
- * For example, given numRows = 5,
- * Run this in the command line:
- * scala PascalTriangle.scala 5
- * Return
- *[
- *     [1],
- *    [1,1],
- *   [1,2,1],
- *  [1,3,3,1],
- * [1,4,6,4,1]
- *]
+ * For example, given numRows = 5, show Pascal's Triangle like this:
+ * [
+ *      [1],
+ *     [1,1],
+ *    [1,2,1],
+ *   [1,3,3,1],
+ *  [1,4,6,4,1]
+ * ]
  */
 
-/**
- * This function takes a commandline 
- * arg which specifies the number of 
- * rows of Pascal's triangle  
- */
-
-
-//class PascalTriangle {
-  object PascalTriangle {
-    def pascaltri (s: String): Array[Array[Int]] = {
+object PascalTriangle {
+  def pascalTriangle (s: String): Array[Array[Int]] = {
     // define rows as a Range traversable: [0,k)
-      val rows = 0 until s.toInt
+    val rows = 0 until s.toInt  
+    /**
+     * define ptri as a 2-D array filled with 1s, e.g.:
+     * [1]
+     * [1,1]
+     * [1,1,1]
+     * [1,1,1,1]
+     * [1,1,1,1,1]
+     */
+    val ptri = rows.toArray map (x => Array.fill(x+1)(1))
   
-  /**
-   * define ptri as a 2-D array filled with 1s, e.g.:
-   *  [1]
-   * [1,1]
-   * [1,1,1]
-   * [1,1,1,1]
-   * [1,1,1,1,1]
-   */
-      val ptri = rows.toArray map (x => Array.fill(x+1)(1))
-  
-  /**
-   * iterate through the 2-D array and 
-   * calculate based on the principle
-   * of Pascal's triangle
-   */
-      if (rows.length >= 2) {
-        for {
-          row <- ptri.indices.init
-          if (row >= 1)
-          col <- ptri(row).indices.init
-        } {
-          ptri(row+1)(col+1) = ptri(row)(col) + ptri(row)(col+1)
+    /**
+     * iterate through the 2-D array and calculate the value of 
+     * each element based on the principle of Pascal's triangle
+     */
+    if (rows.length >= 2) {
+      for {
+        row <- ptri.indices.init
+        if (row >= 1)
+        col <- ptri(row).indices.init
+      } {
+        ptri(row+1)(col+1) = ptri(row)(col) + ptri(row)(col+1)
         }  
-      }
-      ptri
-//  /**
-//   * display Pascal's triangle in
-//   * a specific format
-//   */  
-//      println("[")
-//      for {row <- ptri.indices} {
-//        if (row < ptri.indices.last)
-//      // mkString(start, sep, end)
-//          println(ptri(row).mkString(" "*(ptri.length-row)+"[", ",", "],"))
-//        else
-//          println(ptri(row).mkString(" "*(ptri.length-row)+"[", ",", "]"))
-//      }
-//      println("]")
     }
+    ptri
   }
-//}
-
-//pascaltri(args)
+    
+  def pascalFancyDisplay (pascalTriangle: Array[Array[Int]]) {
+    /**
+     * display Pascal's triangle in a pyramid shape
+     */
+    println("[")
+    for {row <- pascalTriangle.indices} {
+      if (row < pascalTriangle.indices.last)
+        // mkString(start, sep, end)
+        println(pascalTriangle(row).mkString(" "*(pascalTriangle.length-row)+"[", ",", "],"))
+      else
+        println(pascalTriangle(row).mkString(" "*(pascalTriangle.length-row)+"[", ",", "]"))
+    }
+    println("]")  
+  }
+}
--- Triangle.scala
+++ Triangle.scala
@@ -1,20 +1,22 @@
-import PascalTriangle.PascalTriangle.pascaltri
+/**
+ * This Triangle App takes a commandline arg specifying the number of 
+ * rows to display for a Pascal's triangle.
+ * Run "scala Triangle 5" in the commandline will give:
+ * [
+ *      [1],
+ *     [1,1],
+ *    [1,2,1],
+ *   [1,3,3,1],
+ *  [1,4,6,4,1]
+ * ]
+ */
+
+import PascalTriangle.pascalTriangle
+import PascalTriangle.pascalFancyDisplay
 
 object Triangle {
   def main(args: Array[String]) {
-    val ptri = pascaltri(args(0))
-    /**
-   * display Pascal's triangle in
-   * a specific format
-   */  
-    println("[")
-    for {row <- ptri.indices} {
-      if (row < ptri.indices.last)
-      // mkString(start, sep, end)
-        println(ptri(row).mkString(" "*(ptri.length-row)+"[", ",", "],"))
-      else
-        println(ptri(row).mkString(" "*(ptri.length-row)+"[", ",", "]"))
-    }
-    println("]")
+    val ptri = pascalTriangle(args(0))
+    pascalFancyDisplay(ptri)
   }
 }
